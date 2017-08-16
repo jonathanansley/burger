2a0499b0a9f87de2929f382b24572314560dcf67
Jonathan Ansley <ansley.jonathan.e@gmail.com>
2017-08-16 06:44:25 -0400

    all files exist

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (burger_name) VALUES ('Santa Fe');
+INSERT INTO burgers (burger_name) VALUES ('Heart Attack on a Plate');
+INSERT INTO burgers (burger_name) VALUES ('Artery Blocker');
+
+INSERT INTO burgers	(
+		burger_name,
+		devoured)
+VALUES
+	(
+	'Wisconsin Style',
+	true
+	);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('So Cal', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BurgerMeister', true);
+
